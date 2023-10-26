6743abe2b635717a3b6c84fe3abadd1e4d0166c6
Niete <nieteis@gmail.com>
2023-10-26 23:16:54 +0100

    Added script for automatic carousel animation

---
+++ script.js
@@ -0,0 +1,37 @@
+const carousel = document.querySelector('.carousel');
+const images = carousel.querySelectorAll('img');
+const productNames = [
+    'The Micro Bag Collection',
+    'Small Check Merino Wool Scarf',
+    'Mini Alexa in Amethyst',
+    'Medium Lily in Sapphire'
+];
+
+const productName = document.querySelector('.productname');
+let currentIndex = 0;
+
+function changeSlide(newIndex) {
+    currentIndex = newIndex;
+    productName.textContent = productNames[currentIndex];
+    images.forEach((img, index) => {
+        gsap.to(img, {
+            duration: 1.5,
+            opacity: index === currentIndex ? 1: 0 , // Toggle opacity
+            ease: 'power2.inOut'
+        });
+    });
+}
+
+gsap.to(productName, {
+    duration: 1.5,
+    opacity: 1,
+    ease: 'power2.inOut'
+});
+
+changeSlide(currentIndex);
+
+const interval = setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    changeSlide(currentIndex);
+}, 3000);
+
